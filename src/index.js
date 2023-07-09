808c52487a73674f85a6a7e895acfdfeba8f81b0
yuwenzhuo <yuwenzhuo@meituan.com>
2023-07-09 18:53:03 +0800

    100 feat: update test

--- src/index.js
+++ src/index.js
@@ -23,9 +23,46 @@ import WZReact from './WZReact.js'
 // rerender("World")
 
 
-function App(props) {
-  return <h1>Hi {props.name}</h1>
+// function App(props) {
+//   return <h1>Hi {props.name}</h1>
+// }
+// const element = <App name="foo" />
+// const container = document.getElementById("root")
+
+function App(props){
+  return <h1>{props.text}</h1>
 }
-const element = <App name="foo" />
-const container = document.getElementById("root")
-WZReact.render(element, container)
+
+const element1 = (
+  <App text="11111"/>
+)
+
+const element2 = (
+    <App text="2222"/>
+)
+
+const element3 = (
+
+    <App text="3333" />
+
+)
+
+const container = document.getElementById('root')
+const button1 = document.getElementById('button')
+button1.addEventListener('click', onClick)
+
+
+let n = 0
+function onClick() {
+  console.log("=========================================================================================================")
+  if (n === 0) {
+    WZReact.render(element1, container)
+  } else if (n === 1) {
+    WZReact.render(element2, container)
+  } else {
+    WZReact.render(element3, container)
+  }
+  n = (n + 1) % 3
+}
+
+// WZReact.render(element, container)
